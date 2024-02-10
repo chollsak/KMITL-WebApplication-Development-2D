d99d8701b59ff5f4348f9032dcb002fd1283a185
Chollsak <koonnew8688.cookie@gmail.com>
2024-02-11 05:27:58 +0700

    Update

---
+++ act9/worker.js
@@ -0,0 +1,13 @@
+let i = 0;
+function timedCount() {
+  i = i + 1;
+  const a = {
+    'time':new Date(),
+    'num':i
+  };
+  postMessage(a);
+
+  setTimeout("timedCount()",1000);
+}
+
+timedCount();
